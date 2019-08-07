32d10f5dc83558f4bc6fa7997ab0baee138d754a
Daphne Magsby <daphnem@crossbrowsertesting.com>
2019-08-07 12:54:03 -0500

    Initial Commit

---
+++ src/main/java/cbt/test/pageobjects/WikipediaPage.java
@@ -0,0 +1,23 @@
+package cbt.test.pageobjects;
+
+import com.cognifide.qa.bb.page.Page;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import com.cognifide.qa.bb.qualifier.PageObject;
+import com.google.inject.Inject;
+
+@PageObject
+public class WikipediaPage extends Page<WikipediaPage> {
+
+  @Inject
+  private WebDriver webDriver;
+
+  @FindBy(id = "p-search")
+  private SearchComponent searchComponent;
+
+  public SearchComponent getSearchComponent() {
+    return searchComponent;
+  }
+
+}
